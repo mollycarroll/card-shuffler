279c6d5ce8b223692d22cf7cae0fa29f860ebad6
Molly Carroll <mollyajcarroll@gmail.com>
2021-05-22 09:47:30 -0500

    first commit

---
+++ card_shuffler.py
@@ -0,0 +1,27 @@
+import random
+import itertools
+
+values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace']
+suits = ['Hearts', 'Diamonds', 'Spades', 'Clubs']
+
+deck = list(itertools.product(values, suits))
+
+# for value, suit in deck:
+#     print('The %s of %s' % (value, suit))
+
+def shuffle_deck(deck):
+    count = 0
+
+    while count < len(deck):
+        current = deck[random.randint(0, len(deck)-1)]
+
+        if current in deck:
+            deck.remove(current)
+
+        deck.insert(0, current)
+
+        count += 1
+
+    return deck
+
+print(shuffle_deck(deck))
